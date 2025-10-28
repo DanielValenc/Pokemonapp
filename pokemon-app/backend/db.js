31d1646bc2d7b621fc2774a7ed49810543144ea6
DanielValenc <danielvalenciato2002@gmail.com>
2025-10-27 19:27:52 -0500

    Set up a backend and frontend server at the same time

--- pokemon-app/backend/db.js
+++ pokemon-app/backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',     // user
-  host: 'localhost',
+  host: 'pokedesk-n000.onrender.com',
   database: 'pokemon_db', // DB
   password: '1234',
   port: 5432,
